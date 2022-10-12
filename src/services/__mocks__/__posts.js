982f77e527322bd8780815f34b56e2e207cca131
Julie Nisbet <jmnisbet@gmail.com>
2022-10-11 19:24:28 -0700

    remove unused mocks folder

--- src/services/__mocks__/__posts.js
+++
@@ -1,6 +0,0 @@
-export async function getPosts() {
-  return [
-    { id: 1, title: 'Fake Post #1', description: '#1 description' },
-    { id: 2, title: 'Fake Post #2', description: '#2 description' },
-  ];
-}
